d2cc456285b4fd22794d7d020b07e14646671f1f
kenzyxriah <okenlaqahhar@gmail.com>
2023-11-02 00:57:23 +0100

    Add to repo

---
+++ Installation.R
@@ -0,0 +1,44 @@
+# All Installation
+# Most Important Package
+install.packages("tidyverse")
+
+
+
+# For R Markdown
+
+install.packages("base64enc")
+install.packages("digest")
+install.packages("evaluate")
+install.packages("glue")
+install.packages("highr")
+install.packages("htmltools")
+install.packages("jsonlite")
+install.packages("knitr")
+install.packages("magrittr")
+install.packages("markdown")
+install.packages("mime")
+install.packages("rmarkdown")
+install.packages("stringi")
+install.packages("stringr")
+install.packages("xfun")
+install.packages("yaml")
+
+# Other important packages
+install.packages("ggplot2")
+install.packages("janitor")
+install.packages("skimr")
+install.packages("readr")
+install.packages("readxl")
+install.packages("reshape2")
+install.packages("devtools")
+library(devtools)
+devtools::install_github("thomasp85/patchwork")
+
+install.packages("kableExtra")
+install.packages("kable")
+install.packages('reshape2')
+tinytex::install_tinytex()
+
+install.packages("tinytex")
+
+
